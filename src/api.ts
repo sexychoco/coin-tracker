933aa79012e615c9421f992fa018e0c9401d1d58
sexychoco <gaeun819@naver.com>
2023-03-23 18:02:50 +0900

    chart

---
+++ src/api.ts
@@ -0,0 +1,25 @@
+export function fetchCoins() {
+  return fetch("https://api.coinpaprika.com/v1/coins").then((response) =>
+    response.json()
+  );
+}
+
+export function fetchCoinInfo(coinId: string) {
+  return fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`).then(
+    (response) => response.json()
+  );
+}
+
+export function fetchCoinTickers(coinId: string) {
+  return fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`).then(
+    (response) => response.json()
+  );
+}
+
+export function fetchCoinHistory(coinId: string) {
+  const endDate = Math.floor(Date.now() / 1000);
+  const startDate = endDate - 60 * 60 * 24 * 7;
+  return fetch(
+    `https://ohlcv-api.nomadcoders.workers.dev?coinId=${coinId}`
+  ).then((response) => response.json());
+}
